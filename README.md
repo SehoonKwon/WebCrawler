6a9ee9f66113d4a5821c2e2fa3978e7e676d6552
SehoonKwon <rnjstpgns91@naver.com>
2022-03-09 23:04:49 +0900

    study crawler ver2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # WebCrawler
-클론 코딩을 통해 웹 크롤러 실습  
+웹 크롤러 실습  
 webcrawler 코드 내에서 scrapeFolder 사용자 설정에 맞춰서 경로 수정 후 사용
+studyCrawler를 통해 파싱 및 스크래핑 연습
--- studyCrawler.go
+++ studyCrawler.go
@@ -1,6 +1,7 @@
 /*
 대상 웹 사이트 : 정부 코로나 현황
-기능 : 1. 뉴스, 보도자료 링크 및 텍스트 파싱 연습
+기능:	1. 뉴스, 보도자료 링크 및 텍스트 파싱 연습
+		2. 예방접종현황 파싱 (ing)
 */
 package main
 
@@ -19,12 +20,55 @@ func main() {
 	//	fmt.Println(doc.Find("h2.title1").Text()) // 태그 뒤에 class면 . ID면 #
 	//fmt.Println(doc.Find("div.occurrenceStatus").Text())
 
-	//링크 출력 연습, for문이 아니라 Each로 반복문 돌려야 됌
-	//	fmt.Println(doc.Find("ul.m_text_list a[href]").Text())
-	links := doc.Find("ul.m_text_list a[href]")
-	links.Each(func(idx int, sel *goquery.Selection) {
-		link, _ := sel.Attr("href")
-		text := sel.Text()
-		fmt.Println(url+link, text)
+	/*
+		//링크 출력 연습, for문이 아니라 Each로 반복문 돌려야 됌
+		//	fmt.Println(doc.Find("ul.m_text_list a[href]").Text())
+		links := doc.Find("ul.m_text_list a[href]")
+		links.Each(func(idx int, sel *goquery.Selection) {
+			link, _ := sel.Attr("href")
+			text := sel.Text()
+			fmt.Println(url+link, text)
+		})
+	*/
+
+	//예방접종현황
+	var vaccination []string
+	var percentList []string
+	var personCnt []string
+
+	vaccine_list := doc.Find("div.vaccine_list")
+	vaccine_list.Each(func(idx int, sel *goquery.Selection) {
+		/*
+			fmt.Println(sel.Find("div.item").Text())
+			fmt.Println(sel.Find("li.percent").Text())
+			fmt.Println(sel.Find("li.person").Text())
+		*/
+		vac := sel.Find("div.item")
+		perc := sel.Find("li.percent")
+		per := sel.Find("li.person")
+
+		vac.Each(func(idx int, thisSel *goquery.Selection) {
+			vaccination = append(vaccination, thisSel.Text())
+		})
+
+		perc.Each(func(idx int, thisSel *goquery.Selection) {
+			percentList = append(percentList, thisSel.Text())
+		})
+
+		per.Each(func(idx int, thisSel *goquery.Selection) {
+			personCnt = append(personCnt, thisSel.Text())
+		})
 	})
+
+	/*
+		var vaccination []string
+		parse_vaccination := doc.Find("div.vaccine_list div.item")
+		parse_vaccination.Each(func(idx int, sel *goquery.Selection) {
+			vaccination = append(vaccination, sel.Text())
+		})
+	*/
+
+	fmt.Println(vaccination)
+	fmt.Println(percentList)
+	fmt.Println(personCnt)
 }
